c499dca48f6efa8ae514aead6293b18560d1c4c1
Anton Dulnev <akaton@gmail.com>
2024-04-16 14:54:05 +0300

    created the basis of the Hand class

---
+++ hand.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Hand
+  def initialize
+    @cards = []
+  end
+
+  def add_card(card)
+    # добавляем карту в "руку"
+  end
+
+  def score
+    # считаем очки
+  end
+
+  def clear
+    # очищаем карты в руке
+  end
+end

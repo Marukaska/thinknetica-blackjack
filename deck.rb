d6cc31bfef15daaf8ae5e922160d26ccfc35fa3b
Anton Dulnev <akaton@gmail.com>
2024-04-16 14:06:25 +0300

    created the basis of the Deck class

--- deck.rb
+++ deck.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
 class Deck
+  SUITS = %w[♠️ ♥️ ♦️ ♣️]
+  VALUES = %w[2 3 4 5 6 7 8 10 9 В Д К Т]
 
+  def initialize
+    @cards = [] #создаем колоду из объектов класса Card
+  end
+
+  def shuffle!
+    #мешаем карты
+  end
+
+  def deal_card
+    #выдаем карту
+  end
 end

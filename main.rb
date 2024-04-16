8e9f2a9e86239ce9ec9749beddbf0b6ec7ee8c13
Anton Dulnev <akaton@gmail.com>
2024-04-16 14:34:01 +0300

    created the basis of the TextUI class

--- main.rb
+++ main.rb
@@ -1,2 +1,34 @@
 # frozen_string_literal: true
 
+class TextUI
+  def initialize
+    @dealer = Dealer.new
+  end
+
+  def introduction
+    # запрашиваем имя игрока
+    # создаем переменную @player из класса Player
+    # запускаем игру start
+  end
+
+  def start
+    loop do
+      # создаем новую колоду Deck.new
+      # мешаем колоду
+      # выдаем карты
+    end
+  end
+
+  def player_turn
+    # ход игрока
+  end
+
+  def dealer_turn
+    # ход дилера
+  end
+
+  def re_game
+    # считаем итог
+    # предлагаем перезапустить
+  end
+end
